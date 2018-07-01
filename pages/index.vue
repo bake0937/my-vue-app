76fe224b4af02a7b4b7b87cb46288739a3093edb
bake0937 <sadayasu.info@gmail.com>
2018-07-01 17:27:28 +0900

    ページを作成する

---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1 class="title">My Blog App</h1>
+
+    <ul>
+        <li v-for="post in posts">{{post.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: "最初の記事です。"
+        },
+        {
+          id: 2,
+          title: "2番目の投稿です。"
+        },
+        {
+          id: 3,
+          title: "３番めの投稿です。"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
