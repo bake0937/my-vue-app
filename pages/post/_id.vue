4b539427d2bc64522d5221eeb9eba516b74f0221
bake0937 <sadayasu.info@gmail.com>
2018-07-01 18:29:13 +0900

    URL内のパラメータから該当する記事の情報を取得するようにする

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -1,10 +1,29 @@
 <template>
   <div>
-    <h1 class="title">POST TITLE</h1>
-    <p> ここに記事の本文が入ります。 </p>
+    <article v-if="post">
+        <h1 class="title">{{post.title}}</h1>
+        <p> ここに記事の本文が入ります。 </p>
+    </article>
   </div>
 </template>
 
 <script >
-export default {};
+export default {
+  data() {
+    return {
+      post: null
+    };
+  },
+  mounted() {
+    for (let post of this.$store.state.posts) {
+      if (post.id == this.$route.params.id) {
+        this.post = post;
+      }
+    }
+    if (!this.post) {
+      // 記事が存在しない場合トップへ
+      this.$router.push("/");
+    }
+  }
+};
 </script>
