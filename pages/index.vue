5aed76957928c70018ec645d8de85ca8d0d14a84
bake0937 <sadayasu.info@gmail.com>
2018-07-01 18:24:41 +0900

    vuexストアを使う

--- pages/index.vue
+++ pages/index.vue
@@ -11,22 +11,15 @@
 <script>
 export default {
   data() {
-    return {
-      posts: [
-        {
-          id: 1,
-          title: "最初の記事です。"
-        },
-        {
-          id: 2,
-          title: "2番目の投稿です。"
-        },
-        {
-          id: 3,
-          title: "３番めの投稿です。"
-        }
-      ]
-    };
+    return {};
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts;
+    }
+  },
+  mounted() {
+    this.$store.dispatch("load");
   }
 };
 </script>
